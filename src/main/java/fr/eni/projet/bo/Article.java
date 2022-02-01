97c01e5f6990c47c7f9bad18ff97901bdd7d9342
suge49 <nicolas.maillard49320@gmail.com>
2022-02-01 11:31:03 +0100

    class de la bo Article

---
+++ src/main/java/fr/eni/projet/bo/Article.java
@@ -0,0 +1,108 @@
+package fr.eni.projet.bo;
+
+import java.time.LocalDate;
+
+public class Article {
+	//Attribut de la classe Article
+private int noArticle;
+private String nomArticle;
+private String description;
+private LocalDate dateDebutEncheres;
+private LocalDate dateFinEncheres;
+private int prixInitial;
+private int prixVentes;
+
+
+//constructeur par defaut de la classe Article
+public Article() {
+	
+}
+
+
+//coonstructeur surcharger de la classe Article
+public Article(int noArticle,String nomArticle,  String description, LocalDate dateDebutEncheres,
+		LocalDate dateFinEncheres, int prixInitial, int prixVentes) {
+	
+	setNoArticle(noArticle);
+	setNomArticle(nomArticle);
+	setDescription(description);
+	setDateDebutEncheres(dateDebutEncheres);
+	setDateFinEncheres( dateFinEncheres);
+	setPrixInitial(prixInitial);
+	setPrixVentes( prixVentes);
+}
+
+//getter and setter
+public int getNoArticle() {
+	return noArticle;
+}
+
+
+public void setNoArticle(int noArticle) {
+	this.noArticle = noArticle;
+}
+
+public String getNomArticle() {
+	return nomArticle;
+}
+
+public void setNomArticle(String nomArticle) {
+	this.nomArticle = nomArticle;
+}
+
+
+public String getDescription() {
+	return description;
+}
+
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+
+public LocalDate getDateDebutEncheres() {
+	return dateDebutEncheres;
+}
+
+
+public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+	this.dateDebutEncheres = dateDebutEncheres;
+}
+
+
+public LocalDate getDateFinEncheres() {
+	return dateFinEncheres;
+}
+
+
+public void setDateFinEncheres(LocalDate dateFinEncheres) {
+	this.dateFinEncheres = dateFinEncheres;
+}
+
+
+public int getPrixInitial() {
+	return prixInitial;
+}
+
+
+public void setPrixInitial(int prixInitial) {
+	this.prixInitial = prixInitial;
+}
+
+
+public int getPrixVentes() {
+	return prixVentes;
+}
+
+
+public void setPrixVentes(int prixVentes) {
+	this.prixVentes = prixVentes;
+}
+
+
+
+
+
+
+}

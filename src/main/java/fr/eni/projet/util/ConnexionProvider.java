bd8943e6f24b6145f5958d8b98bfe6a66cc03d2d
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-01 11:35:49 +0100

    Création de la connexion avec :
    le context.xml
    le web.xml

--- src/main/java/fr/eni/projet/util/ConnexionProvider.java
+++ src/main/java/fr/eni/projet/util/ConnexionProvider.java
@@ -1,5 +1,28 @@
 package fr.eni.projet.util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ConnexionProvider {
 
+	private static DataSource datasource;
+
+	static {
+		try {
+			Context context = new InitialContext();
+			datasource = (DataSource) context.lookup("java:comp/env/jdbc/RepasCNXPool");
+		} catch (NamingException e) {
+			//TODO gestion d'erreur à faire
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return datasource.getConnection();
+	}
 }

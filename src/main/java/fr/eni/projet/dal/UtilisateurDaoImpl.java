5302f01e9a31119eee572c19dcc97c73467741cc
naelalaji <58527507+naelalaji@users.noreply.github.com>
2022-02-01 16:53:04 +0100

    MAJ utilisateur

--- src/main/java/fr/eni/projet/dal/UtilisateurDaoImpl.java
+++ src/main/java/fr/eni/projet/dal/UtilisateurDaoImpl.java
@@ -1,5 +1,30 @@
 package fr.eni.projet.dal;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+import fr.eni.projet.bo.Utilisateur;
+import fr.eni.projet.util.ConnexionProvider;
+
 public class UtilisateurDaoImpl implements UtilisateurDAO {
+	
+	private final static String SELECT_BY_PSEUDO = "SELECT * FROM UTILISATEURS WHERE pseudo = 1;";
+	
+	@Override
+	public void newUtilisateur(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override //a faire  Par nael
+	public Utilisateur selectByPseudo(Utilisateur utilisateur) {
+		Connection cnx = ConnexionProvider.getConnection();
+		PreparedStatement pstmt =cnx.prepareStatement(SELECT_BY_PSEUDO)
+		ResultSet rs = pstmt.executeQuery();
+		return null;
+	}
 
 }

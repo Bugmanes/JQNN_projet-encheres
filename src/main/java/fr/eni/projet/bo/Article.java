1862e14c7d5e14c11057b2209b8136ba46f86eea
suge49 <nicolas.maillard49320@gmail.com>
2022-02-01 13:13:33 +0100

    Mise a jour de l classe article

--- src/main/java/fr/eni/projet/bo/Article.java
+++ src/main/java/fr/eni/projet/bo/Article.java
@@ -1,108 +1,90 @@
 package fr.eni.projet.bo;
 
 import java.time.LocalDate;
-
+//class Nicolas
 public class Article {
-	//Attribut de la classe Article
-private int noArticle;
-private String nomArticle;
-private String description;
-private LocalDate dateDebutEncheres;
-private LocalDate dateFinEncheres;
-private int prixInitial;
-private int prixVentes;
-
+	// Attribut de la classe Article
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private int prixInitial;
+	private int prixVentes;
+	private Retrait retrait;
 
 //constructeur par defaut de la classe Article
-public Article() {
-	
-}
+	public Article() {
 
+	}
 
 //coonstructeur surcharger de la classe Article
-public Article(int noArticle,String nomArticle,  String description, LocalDate dateDebutEncheres,
-		LocalDate dateFinEncheres, int prixInitial, int prixVentes) {
-	
-	setNoArticle(noArticle);
-	setNomArticle(nomArticle);
-	setDescription(description);
-	setDateDebutEncheres(dateDebutEncheres);
-	setDateFinEncheres( dateFinEncheres);
-	setPrixInitial(prixInitial);
-	setPrixVentes( prixVentes);
-}
+	public Article(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			int prixInitial, int prixVentes) {
+
+		setNomArticle(nomArticle);
+		setDescription(description);
+		setDateDebutEncheres(dateDebutEncheres);
+		setDateFinEncheres(dateFinEncheres);
+		setPrixInitial(prixInitial);
+		setPrixVentes(prixVentes);
+	}
 
 //getter and setter
-public int getNoArticle() {
-	return noArticle;
-}
+	public int getNoArticle() {
+		return noArticle;
+	}
 
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
 
-public void setNoArticle(int noArticle) {
-	this.noArticle = noArticle;
-}
+	public String getNomArticle() {
+		return nomArticle;
+	}
 
-public String getNomArticle() {
-	return nomArticle;
-}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
 
-public void setNomArticle(String nomArticle) {
-	this.nomArticle = nomArticle;
-}
+	public String getDescription() {
+		return description;
+	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-public String getDescription() {
-	return description;
-}
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
 
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
 
-public void setDescription(String description) {
-	this.description = description;
-}
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
 
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
 
-public LocalDate getDateDebutEncheres() {
-	return dateDebutEncheres;
-}
+	public int getPrixInitial() {
+		return prixInitial;
+	}
 
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
 
-public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
-	this.dateDebutEncheres = dateDebutEncheres;
-}
+	public int getPrixVentes() {
+		return prixVentes;
+	}
 
-
-public LocalDate getDateFinEncheres() {
-	return dateFinEncheres;
-}
-
-
-public void setDateFinEncheres(LocalDate dateFinEncheres) {
-	this.dateFinEncheres = dateFinEncheres;
-}
-
-
-public int getPrixInitial() {
-	return prixInitial;
-}
-
-
-public void setPrixInitial(int prixInitial) {
-	this.prixInitial = prixInitial;
-}
-
-
-public int getPrixVentes() {
-	return prixVentes;
-}
-
-
-public void setPrixVentes(int prixVentes) {
-	this.prixVentes = prixVentes;
-}
-
-
-
-
-
+	public void setPrixVentes(int prixVentes) {
+		this.prixVentes = prixVentes;
+	}
 
 }

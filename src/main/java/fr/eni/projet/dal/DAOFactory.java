85b3f33f2a2ee2a5e524c3cbeee5d50eddc7db76
suge49 <nicolas.maillard49320@gmail.com>
2022-02-02 10:31:07 +0100

    Maj de la daoFactory Nicolas

--- src/main/java/fr/eni/projet/dal/DAOFactory.java
+++ src/main/java/fr/eni/projet/dal/DAOFactory.java
@@ -3,10 +3,11 @@ package fr.eni.projet.dal;
 public class DAOFactory {
 
 	//methode de création d'un utilisateur
-	public static UtilisateurDAO createUtilisateurDAO() {
+	public static UtilisateurDAO getUtilisateurDAO() {
 		
+	UtilisateurDAO utilisateurDAo = new UtilisateurDaoImpl(); 
 		//me return une new instance d'Utilisateur qui implémente les methode de UtilisateurDAO
-		return new UtilisateurDaoImpl();
+		return  utilisateurDAo;
 	}
 	
 	

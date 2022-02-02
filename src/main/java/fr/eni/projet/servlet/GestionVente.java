55ae20e43391eedb938266e64c38cadb30ab5c65
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-02 16:16:33 +0100

    maj jsp et servlet nouvelle vente

--- src/main/java/fr/eni/projet/servlet/GestionVente.java
+++ src/main/java/fr/eni/projet/servlet/GestionVente.java
@@ -4,13 +4,21 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.jni.File;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 import fr.eni.projet.bll.CategorieManager;
 import fr.eni.projet.bll.EnchereManager;
@@ -41,16 +49,27 @@ public class GestionVente extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// créer une vente
+		
+		// récupération des paramètres
 		String nom = request.getParameter("nomArticle");
 		String description = request.getParameter("description");
 		int miseAPrix = Integer.parseInt(request.getParameter("prixInitial"));
 		LocalDate dateDebut = LocalDate.parse(request.getParameter("debut"));
 		LocalDate dateFin = LocalDate.parse(request.getParameter("fin"));
-		HttpSession session = request.getSession();
-		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+		
+		// récupération de la catégorie
 		String libelle = request.getParameter("categorie");
 		CategorieManager cm = CategorieManager.getInstance();
 		Categorie categorie = cm.chercherCategorie(libelle);
+		
+		// récupération d'utilisateur de la session
+		HttpSession session = request.getSession();
+		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
+		
+		// récupération de l'image de l'article
+		
+		
+		// envoi à la bll
 		EnchereManager em = EnchereManager.getInstance();
 		em.nouvelleVente(nom, description, dateDebut, dateFin, miseAPrix, utilisateur, categorie);
 		

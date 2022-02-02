0e5ccbad13e2e235635b41390171aad2a5c01a1e
suge49 <nicolas.maillard49320@gmail.com>
2022-02-02 11:18:45 +0100

    Maj Utilisateurmanager avec le Siingleton Nicolas

--- src/main/java/fr/eni/projet/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projet/bll/UtilisateurManager.java
@@ -11,35 +11,21 @@ public class UtilisateurManager {
 	private UtilisateurManager() {
 
 	}
-	
-	private UtilisateurManager(String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
 
-	}
-
-	public static UtilisateurManager getInstance(String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville, String motDePasse, int credit, boolean administrateur)   {
+	public static UtilisateurManager getInstance() {
 		if (instance == null) {
-			instance = new UtilisateurManager(pseudo,nom, prenom,email,telephone,rue,codePostal,ville,motDePasse,credit,administrateur);
-			UtilisateurDAO dao = DAOFactory.getUtilisateurDAO();
-			dao.getUtilisateurDAO(UtilisateurManager);
-		
+			instance = new UtilisateurManager();
 		}
-		
 		return instance;
 	}
 
-	//			( {
-//
-//				Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville,
-//						motDePasse, credit, administrateur);
-//
-//				UtilisateurDAO dao = DAOFactory.getUtilisateurDAO();
-//				dao.newUtilisateur(utilisateur);
-//			}
-//		
-		
-		
-		
+	public void nouvelleUtilisateur(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		Utilisateur user = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse,
+				credit, administrateur);
+		UtilisateurDAO userDAO = DAOFactory.getUtilisateurDAO();
+		userDAO.newUtilisateur(user);
+
+	}
 
 }

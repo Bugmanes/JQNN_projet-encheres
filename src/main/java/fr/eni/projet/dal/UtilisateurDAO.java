0f067b320c24680da27e6528f2185ac02dd04da7
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-02 08:07:51 +0100

    maj UtilisateurDAO

--- src/main/java/fr/eni/projet/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/projet/dal/UtilisateurDAO.java
@@ -4,9 +4,10 @@ import fr.eni.projet.bo.Utilisateur;
 
 public interface UtilisateurDAO {
 
-//methode pour créer un nouvelle utlisateur
+	//methode pour créer un nouvelle utlisateur
 	public void newUtilisateur(Utilisateur utilisateur);
 
+	// méthode pour sélectionner un utilisateur par son pseudo
 	public abstract Utilisateur selectByPseudo(Utilisateur utilisateur);
 
 }

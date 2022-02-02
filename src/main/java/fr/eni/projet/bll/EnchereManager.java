18d20a0d54d70186bdeaa3ba6dfb134fd0e55252
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-02 09:32:56 +0100

    creation des singleton

--- src/main/java/fr/eni/projet/bll/EnchereManager.java
+++ src/main/java/fr/eni/projet/bll/EnchereManager.java
@@ -1,5 +1,18 @@
 package fr.eni.projet.bll;
 
 public class EnchereManager {
+	
+	private static EnchereManager instance;
+	
+	private EnchereManager() {
+
+	}
+	
+	public static EnchereManager getInstance() {
+		if (instance == null) {
+			instance = new EnchereManager();
+		}
+		return instance;
+	}
 
 }
--- src/main/java/fr/eni/projet/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projet/bll/UtilisateurManager.java
@@ -6,14 +6,27 @@ import fr.eni.projet.dal.UtilisateurDAO;
 
 public class UtilisateurManager {
 
-public void ajouterUtilisateur (String pseudo, String nom, String prenom, String email, String telephone, String rue,
+	private static UtilisateurManager instance;
+
+	private UtilisateurManager() {
+
+	}
+
+	public static UtilisateurManager getInstance() {
+		if (instance == null) {
+			instance = new UtilisateurManager();
+		}
+		return instance;
+	}
+
+	public void ajouterUtilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
 			String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
-	
-	Utilisateur utilisateur = new Utilisateur( pseudo,  nom,  prenom,  email,  telephone,  rue,
-			 codePostal,  ville,  motDePasse, credit, administrateur);
-	
-	UtilisateurDAO dao = DAOFactory.createUtilisateurDAO();
-	dao.newUtilisateur(utilisateur);
-}
-	
+
+		Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville,
+				motDePasse, credit, administrateur);
+
+		UtilisateurDAO dao = DAOFactory.createUtilisateurDAO();
+		dao.newUtilisateur(utilisateur);
+	}
+
 }

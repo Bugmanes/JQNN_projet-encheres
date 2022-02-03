7c7a51a4097aa9dfdce1605522399d2d3b271866
seth- <seth-@192.168.1.103>
2022-02-03 18:43:17 +0100

    suppresion des méthodes inutiles mises en commentaire dans DAOFactory

--- src/main/java/fr/eni/projet/dal/DAOFactory.java
+++ src/main/java/fr/eni/projet/dal/DAOFactory.java
@@ -6,34 +6,28 @@ public class DAOFactory {
 	public static UtilisateurDAO getUtilisateurDAO() {
 
 		UtilisateurDAO utilisateurDAo = new UtilisateurDaoImpl();
-		// me return une new instance d'Utilisateur qui implémente les methodes de
-		// UtilisateurDAO
 		return utilisateurDAo;
 	}
 
 	// méthode de création d'un catégorie DAO
 	public static CategorieDAO getCategorieDAO() {
+		
 		CategorieDAO categorieDAO = new CategorieDaoImpl();
 		return categorieDAO;
 	}
 
 	// méthode de création d'un enchereDAO
 	public static EnchereDAO getEnchereDAO() {
+		
 		EnchereDAO enchereDAO = new EnchereDaoImpl();
 		return enchereDAO;
 	}
 
 	// méthode de création d'un articleDAO
 	public static ArticleDAO getArticleDAO() {
+		
 		ArticleDAO art = new ArticleDaoImpl();
 		return art;
 	}
 
-//	public static UtilisateurDAO selectUtilisateur() {
-//
-//		UtilisateurDAO utilisateurDAo = new UtilisateurDaoImpl();
-//		// me return une new instance d'Utilisateur qui implémente les methodes de
-//		// UtilisateurDAO
-//		return utilisateurDAo;
-//	}
 }

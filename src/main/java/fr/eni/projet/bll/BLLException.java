1e09fcc53dea474929e11bab094deebd34a7cb56
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-04 08:07:28 +0100

    azerty

---
+++ src/main/java/fr/eni/projet/bll/BLLException.java
@@ -0,0 +1,27 @@
+package fr.eni.projet.bll;
+
+	public class BLLException extends Exception {
+
+		// Constructeurs
+		public BLLException() {
+			super();
+		}
+
+		public BLLException(String message) {
+			super(message);
+		}
+
+		public BLLException(String message, Throwable exception) {
+			super(message, exception);
+		}
+
+		// Methodes
+		@Override
+		public String getMessage() {
+			StringBuilder sb = new StringBuilder("Couche DAL - ");
+			sb.append(super.getMessage());
+			return sb.toString();
+		}
+	}
+
+

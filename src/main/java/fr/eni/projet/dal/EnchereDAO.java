f4b64225b3110b4268f2e4a5b15d1dfbb498a748
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-07 10:38:52 +0100

    petite maj

--- src/main/java/fr/eni/projet/dal/EnchereDAO.java
+++ src/main/java/fr/eni/projet/dal/EnchereDAO.java
@@ -1,6 +1,9 @@
 package fr.eni.projet.dal;
 
-public interface EnchereDAO {
-	public void deleteEnchere(int numUtil) throws DALException;
+import fr.eni.projet.bo.Enchere;
 
+public interface EnchereDAO {
+	
+	public void deleteEnchere(int numUtil) throws DALException;
+	public void insertEnchere(Enchere enchere) throws DALException;
 }
--- src/main/java/fr/eni/projet/dal/EnchereDaoImpl.java
+++ src/main/java/fr/eni/projet/dal/EnchereDaoImpl.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import fr.eni.projet.bo.Enchere;
 import fr.eni.projet.bo.Utilisateur;
 import fr.eni.projet.util.ConnexionProvider;
 
 public class EnchereDaoImpl implements EnchereDAO {
 
 	private static final String DELETE_ENCHERE = "DELETE FROM ENCHERES where no_utilisateur = ?;";
+	private static final String INSERT_ENCHERE = "INSERT INTO ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere) VALUES (?, ?, ?, ?);";
 
 	@Override
 	public void deleteEnchere(int numUtil) throws DALException {
@@ -23,6 +25,12 @@ public class EnchereDaoImpl implements EnchereDAO {
 		} catch (SQLException e) {
 			throw new DALException("problem de deleteEnchere ", e);
 		}
+	}
+
+	@Override
+	public void insertEnchere(Enchere enchere) throws DALException {
+		// TODO Auto-generated method stub
+		
 	}
 
 	

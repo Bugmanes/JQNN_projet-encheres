c64e2293b3de65db5c7bd9d355bebab27b9034ab
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-07 11:16:42 +0100

    pour nael

--- src/main/java/fr/eni/projet/servlet/ModifierProfil.java
+++ src/main/java/fr/eni/projet/servlet/ModifierProfil.java
@@ -36,79 +36,78 @@ public class ModifierProfil extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//Récupère la session
+
+		// recuperation la session
 		HttpSession session = request.getSession();
 		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
-		request.setAttribute("Utilisateur", utilisateur);
-		UtilisateurManager um = UtilisateurManager.getInstance();
-		
-		String choix = request.getParameter("choix");
-		request.setAttribute("choix", choix);
-		if (choix.equals("supprimer")) {
-						request.setAttribute("suppression", "Votre compte vient d'etre supprimer");
-						this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/accueil.jsp").forward(request, response);
-						
-						
-		} else  {
-						request.setAttribute("Valider", "Votre profil est modifi�");
-						this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/monProfil.jsp").forward(request, response);
-						// parametrage de la requete
-						String pseudo = request.getParameter("pseudo").trim();
-						String nom = request.getParameter("nom").trim();
-						String prenom = request.getParameter("prenom").trim();
-						String email = request.getParameter("email").trim();
-						String telephone = request.getParameter("telephone").trim();
-						String rue = request.getParameter("rue").trim();
-						String codePostal = request.getParameter("code_postal").trim();
-						String ville = request.getParameter("ville").trim();
-						String old_mdp = request.getParameter("old_mdp").trim();
-						String new_mdp = request.getParameter("new_mdp").trim();
-			
-						boolean pseudoOK = false;
-						boolean nomOK = false;
-						boolean prenomOK = false;
-						boolean telOK = false;
-						boolean pseudoUniqueOK = false;
-						boolean emailUniqueOK = false;
-			
-						if (!utilisateur.getPseudo().equals(pseudo)) {
-							pseudoOK = um.verifPseudo(pseudo);
-							try {
-								pseudoUniqueOK = um.verifUniquePseudo(pseudo);
-							} catch (DALException e) {
-								System.err.println(e.getMessage());
-							}
-						}
-						if (!utilisateur.getNom().equals(nom)) {
-							nomOK = um.verifNomPrenom(nom);
-						}
-						if (!utilisateur.getPrenom().equals(prenom)) {
-							prenomOK = um.verifNomPrenom(prenom);
-						}
-						if (!utilisateur.getTelephone().equals(telephone)) {
-							telOK = um.verifTel(telephone);
-						}
-						if (!utilisateur.getEmail().equals(email)) {
-							emailUniqueOK = false;
-						}
-			
-						if (!pseudoOK || !nomOK || !prenomOK || !telOK || !pseudoUniqueOK || !emailUniqueOK)
-							request.setAttribute("pseudoOK", pseudoOK);
-						request.setAttribute("pseudoUniqueOK", pseudoUniqueOK);
-						request.setAttribute("nomOK", nomOK);
-						request.setAttribute("prenomOK", prenomOK);
-						request.setAttribute("telOK", telOK);
-						request.setAttribute("emailUniqueOK", emailUniqueOK);
-						request.getRequestDispatcher("/WEB-INF/jsp/monProfil.jsp").forward(request, response);
-		
 
-		try {
-			// insertion de parametres dans modifierUtilisateur
-			um.modifierUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, ville, utilisateur);
-        } 
-		catch (DALException e) {
-			System.err.println(e.getMessage());
-		}
+		UtilisateurManager um = UtilisateurManager.getInstance();
+
+		String choix = request.getParameter("choix").trim();
+
+		if (choix.equals("supprimer")) {
+			request.setAttribute("suppression", "Votre compte vient d'etre supprimer");
+			this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/accueil.jsp").forward(request, response);
+
+		} else {
+			request.setAttribute("Valider", "Votre profil est modifie");
+			this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/monProfil.jsp").forward(request, response);
+			// parametrage de la requete
+			String pseudo = request.getParameter("pseudo").trim();
+			String nom = request.getParameter("nom").trim();
+			String prenom = request.getParameter("prenom").trim();
+			String email = request.getParameter("email").trim();
+			String telephone = request.getParameter("telephone").trim();
+			String rue = request.getParameter("rue").trim();
+			String codePostal = request.getParameter("code_postal").trim();
+			String ville = request.getParameter("ville").trim();
+			String old_mdp = request.getParameter("old_mdp").trim();
+			String new_mdp = request.getParameter("new_mdp").trim();
+
+			boolean pseudoOK = false;
+			boolean nomOK = false;
+			boolean prenomOK = false;
+			boolean telOK = false;
+			boolean pseudoUniqueOK = false;
+			boolean emailUniqueOK = false;
+
+			if (!utilisateur.getPseudo().equals(pseudo)) {
+				pseudoOK = um.verifPseudo(pseudo);
+				try {
+					pseudoUniqueOK = um.verifUniquePseudo(pseudo);
+				} catch (DALException e) {
+					System.err.println(e.getMessage());
+				}
+			}
+			if (!utilisateur.getNom().equals(nom)) {
+				nomOK = um.verifNomPrenom(nom);
+			}
+			if (!utilisateur.getPrenom().equals(prenom)) {
+				prenomOK = um.verifNomPrenom(prenom);
+			}
+			if (!utilisateur.getTelephone().equals(telephone)) {
+				telOK = um.verifTel(telephone);
+			}
+			if (!utilisateur.getEmail().equals(email)) {
+				emailUniqueOK = false;
+			}
+
+			if (!pseudoOK || !nomOK || !prenomOK || !telOK || !pseudoUniqueOK || !emailUniqueOK)
+				request.setAttribute("pseudoOK", pseudoOK);
+			request.setAttribute("pseudoUniqueOK", pseudoUniqueOK);
+			request.setAttribute("nomOK", nomOK);
+			request.setAttribute("prenomOK", prenomOK);
+			request.setAttribute("telOK", telOK);
+			request.setAttribute("emailUniqueOK", emailUniqueOK);
+			request.getRequestDispatcher("/WEB-INF/jsp/monProfil.jsp").forward(request, response);
+
+			try {
+				// insertion de parametres dans modifierUtilisateur
+				um.modifierUtilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, ville,
+						utilisateur);
+			} catch (DALException e) {
+				System.err.println(e.getMessage());
+			}
 		}
 	}
 }

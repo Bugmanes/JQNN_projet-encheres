f4c9e9c381dda1f7b28238a23f4bb9bc64b6137d
jvelu2021 <jvelu2021@177SE44-80JBC5J.ad.campus-eni.fr>
2022-02-08 16:43:52 +0100

    maj

--- src/main/java/fr/eni/projet/servlet/Connexion.java
+++ src/main/java/fr/eni/projet/servlet/Connexion.java
@@ -19,7 +19,7 @@ public class Connexion extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		request.getRequestDispatcher("/WEB-INF/jsp/seConnecter.jsp").forward(request, response);
 
 	}
